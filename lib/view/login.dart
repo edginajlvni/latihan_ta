09e4b9a258542f22e58d8221a5df79e0559fdc0a
ed.ginaa <113158996+edginajlvni@users.noreply.github.com>
2024-01-23 14:02:44 +0700

    2

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,22 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_ta/view/splashview.dart';
 
-class LogIn extends StatelessWidget{
-
+class LogIn extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
-      // body: Container(
-      //   width: double.infinity,
-      //   decoration: BoxDecoration(
-      //     gradient: LinearGradient(begin: Alignment.topCenter,
-      //     colors: [
-      //       Colors.pink
-      //     ])
-      //   ),
-      // ),
-    );
+      backgroundColor: const Color.fromARGB(108, 231, 231, 231),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 25, left: 15, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Shallom",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: "Ruluko",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 1),
+            const Padding(
+              padding: EdgeInsets.only(right: 25, left: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Silakan Login",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
 
+            const SizedBox(height: 15),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Fasilitas",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // SizedBox(height: 10),
+            // SingleChildScrollView(
+            //   scrollDirection: Axis.horizontal,
+            // child:
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+              child: Row(children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 165, 161, 161),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(115, 211, 199, 199),
+                          )
+                        ]),
+                    child: Text(
+                      "Toilet",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 165, 161, 161),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(115, 211, 199, 199),
+                          )
+                        ]),
+                    child: Text(
+                      "Tribun",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 165, 161, 161),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(115, 211, 199, 199),
+                          )
+                        ]),
+                    child: Text(
+                      "Warung",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 165, 161, 161),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(115, 211, 199, 199),
+                          )
+                        ]),
+                    child: Text(
+                      "Bola",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            InkWell(
+              onTap: () {},
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Splash()));
+                },
+                child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(104, 11, 1, 12),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                      child: Text("Booking",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          )),
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
